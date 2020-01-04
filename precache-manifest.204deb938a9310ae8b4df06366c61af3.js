483bbb26cae0cfa77434d2008f9ea7192892735d
Toan <nguyentoan8434@gmail.com>
2020-01-04 22:03:26 +0700

    Init commit

---
+++ precache-manifest.204deb938a9310ae8b4df06366c61af3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "51c198d33b7916bc6e0e",
+    "url": "/static/js/main.c922edef.chunk.js"
+  },
+  {
+    "revision": "c9396484d357007d91c8",
+    "url": "/static/js/2.82e1afb4.chunk.js"
+  },
+  {
+    "revision": "f987e864d2af32ca85b85328b934f5b7",
+    "url": "/index.html"
+  }
+];
